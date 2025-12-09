b1c04471be15c7f2563ab68daeff76ec522cfb86
Zaheer <zaheer.ahmed@corbinsoft.com>
2025-12-09 16:42:25 +0530

    updated again

--- testj.java
+++ testj.java
@@ -1,11 +1,11 @@
 /*
- * @wi.implements drivepilot DP-439: Hello world implementation
- * This class implements logic related to Software Requirement DP-440
+ * @wi.DP-439
+ * 
  */
 
 public class HelloWorld {
 
-    // POLARION-ID: DP-439
+    // POLARION-ID: @wi.DP-439
     public static void sayHello() {
         System.out.println("Hello, World!");
     }
