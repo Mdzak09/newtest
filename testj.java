267b6bb6a539865a9d63e67bc11c892a2558f4f4
Zaheer <zaheer.ahmed@corbinsoft.com>
2025-12-09 16:11:01 +0530

    aaja bail pls

---
+++ testj.java
@@ -0,0 +1,16 @@
+/*
+ * POLARION-ID: DP-439
+ * This class implements logic related to Software Requirement DP-440
+ */
+
+public class HelloWorld {
+
+    // POLARION-ID: DP-439
+    public static void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
