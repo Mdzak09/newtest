503d79e4b66847c5000fdcaf647f484537a1b146
md zaheer ahmed khan <89810240+Mdzak09@users.noreply.github.com>
2025-12-09 15:22:59 +0530

    Add files via upload

---
+++ text.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * POLARION-ID: DP-439
+ * This function is linked to the Software Requirement DP-440
+ */
+
+void sayHello() {
+    printf("Hello, World!\n");
+}
+
+int main() {
+    sayHello();
+    return 0;
+}
